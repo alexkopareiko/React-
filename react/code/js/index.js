3b331d6a185e205aa084ad1bd38bd13b2013caca
Alexandr Kopareiko <sanyakopar@gmail.com>
2018-06-06 20:51:00 +0300

    Урок Redux 3
    Хранилище и преобразователи

--- react/code/js/index.js
+++ react/code/js/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+import allReducers from './reducers';
+
+const store = createStore (allReducers);
 
 ReactDOM.render(
-  <h2>examplsdf</h2>, 
+  <h2>examplsdf</h2>,
   document.getElementById('fieldToShow')
 );
