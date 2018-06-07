6c6d40bb107153f0ae0a37393941668171ff74da
Alexandr Kopareiko <sanyakopar@gmail.com>
2018-06-07 08:34:52 +0300

    Урок 5 Контейнеры

--- react/code/js/components/WebPage.js
+++ react/code/js/components/WebPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CarsList from '../containers/car-list';
 //можно добавить css../../
 
 const WebPage = () => (
   <div>
     <h2>Cars:</h2>
+    <CarsList />
     <hr />
     <h3>Details:</h3>
   </div>
---
+++ react/code/js/containers/car-list.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+class CarsList extends Component {
+  render () {
+    return (
+      <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ol>
+    );
+  }
+}
+
+function mapStateToProps (state){
+  return {
+    cars: state.cars
+  };
+}
+
+export default connect(mapStateToProps)(CarsList);
